eaac5790716384758491c05a9c58705303556d0a
Ana Garcia <anagdesignweb@gmail.com>
2024-04-28 15:16:28 +0200

    Finished tutorial 1: Component States Hook useState()

--- proyects/00-hola-mundo/src/App.jsx
+++ proyects/00-hola-mundo/src/App.jsx
@@ -4,8 +4,17 @@ import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 export function App () {
     return (
         <section className='App'>
-            <TwitterFollowCard isFollowing userName="anana" name="Ana Garcia" />
-            <TwitterFollowCard isFollowing={false} userName="elonmusk" name="Elon Musk" />          
+            <TwitterFollowCard 
+                userName="anana" 
+                name="Ana Garcia"
+                initialIsFollowing={true}
+            />
+
+            <TwitterFollowCard 
+                userName="elonmusk" 
+                name="Elon Musk"
+                initialIsFollowing={false}
+            />          
         </section>
     )
 }
--- proyects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ proyects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,4 +1,16 @@
-export function TwitterFollowCard ({userName, name, isFollowing}) {
+import { useState } from 'react'
+
+export function TwitterFollowCard ({userName, name, initialIsFollowing}) {
+    // aqui abajo usamos el hook useState, este devuelve un objeto, el primero es el valor, y el segundo la forma de actualizar el estado(el cual usamos despues en la funcion handleclick para cambiar el estado inicial) el parametro que le pasamos al useState es el valor por defecto de isFollowing, aqui podemos pasarle directamente un parametro, o como estaria mejro hecho que es pasandole una prop, en este caso le vamos a dar una prop inicial al componente en APP, asi inicializara con el valor que tenga ese componente.
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+    // estas dos funciones de abajo son para cambiar las classes de css segun el estado.
+    const text = isFollowing ? 'siguiendo' : 'seguir'
+    const buttonClassName = isFollowing ? 'tw-followCard-btn is-following' : 'tw-followCard-btn'
+
+    const handleClick = () => { //esta funcion es la que se le pasa al onclick para que cambie el estado.
+        setIsFollowing(!isFollowing)
+    }
+
     return (
         <article className='tw-followCard'>
         <header className='tw-followCard-header'>
@@ -9,8 +21,8 @@ export function TwitterFollowCard ({userName, name, isFollowing}) {
             </div>
         </header>
         <aside>
-            <button className='tw-followCard-btn'>
-                Seguir
+            <button className={buttonClassName} onClick={handleClick}>
+                {text}
             </button>
         </aside>
     </article>
