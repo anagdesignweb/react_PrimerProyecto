53f7af9c8ef07f5c0379144202b07ee79a5861bf
Ana Garcia <anagdesignweb@gmail.com>
2024-08-04 14:20:50 +0200

    Primer repaso con el proyecto 00-hola-mundo

--- proyects/00-hola-mundo/src/App.jsx
+++ proyects/00-hola-mundo/src/App.jsx
@@ -1,20 +1,57 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
+
+const users = [
+    {
+        userName:'midudev',
+        name:'Miguel Angel Duran',
+        isFollowing: true
+    },
+    {
+        userName:'pacoHdezs',
+        name:'Paco H.',
+        isFollowing: false
+    },
+    {
+        userName:'nananan',
+        name:'Nanai',
+        isFollowing: true
+    },
+]
+
+
 export function App () {
     return (
         <section className='App'>
-            <TwitterFollowCard 
-                userName="anana" 
+
+        {   /* Aqui vamos a hacer un bucle para mostrar los diferentes usuarios del array users */
+            users.map(user => {
+                const {userName, name, isFollowing} = user
+                return (
+                    <TwitterFollowCard
+                        userName={userName}
+                        name={name}
+                        initialIsFollowing={isFollowing}
+                    >
+                        {name}
+                     </TwitterFollowCard>
+                )
+            })
+        }
+
+
+            <TwitterFollowCard
+                userName="anana"
                 name="Ana Garcia"
                 initialIsFollowing={true}
             />
 
-            <TwitterFollowCard 
-                userName="elonmusk" 
+            <TwitterFollowCard
+                userName="elonmusk"
                 name="Elon Musk"
                 initialIsFollowing={false}
-            />          
+            />
         </section>
     )
 }
